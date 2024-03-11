2df4450a6ca623443d93364c1291b4e77349822a
AtayevZarif <atayev1234567890@gmail.com>
2024-03-11 22:32:28 +0500

    js project

---
+++ main.js
@@ -0,0 +1,13 @@
+const numberOne = prompt("Ismingizni kiriting");
+const numberTwo = prompt("Familiyangizni kiriting")
+let numberThree = prompt("Yoshingizni kiriting")
+let numberFour = prompt("Yoqtirgan faningizni kiriting")
+var numberFive = prompt("Manzilingizni kiriting")
+var end = "Malumotingizni kiritganingiz uchun tashakkur"
+
+console.log(numberOne)
+alert(numberTwo)
+console.log(numberThree)
+alert(numberFour)
+console.log(numberFive)
+alert(end)
